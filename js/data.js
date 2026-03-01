57d4dd7ad974f3afb8be641247012ad260e52bc1
aymanfouad123 <aymanfouad033@gmail.com>
2026-03-01 15:36:36 -0500

    chore: added ayman fouad to cs-webring + updated data.js formatting to unqoted keys (standard style with formatters like prettier etc.)

--- js/data.js
+++ js/data.js
@@ -1,40 +1,49 @@
 // sites
 
 const webringData = {
-    "sites": [
-        {
-            "name": "Anita Jiang",
-            "year": "2028",
-            "website": "https://anitajiang.me",
-            "socials": {
-            "linkedin": "https://www.linkedin.com/in/antjng/",
-            "x": "https://x.com/antjng_"
-        }
-        },
-        {
-        "name": "Shadi El-Fares",
-        "year": "2027",
-        "website": "https://shadielfares.com",
-        "socials": {
-            "linkedin": "https://www.linkedin.com/in/shadielfares/",
-            "x": "https://x.com/shadielfares"
-        }
-        }
-        // add new sites above this comment
-        // for clarity: year refers to your projected graduation year.
-        // follow this format:
-        // {
-        //     "name": "Your Name",
-        //     "year": "20XX",
-        //     "website": "https://yourwebsite.com",
-        //     "socials": {
-        //         "linkedin": "https://www.linkedin.com/in/yourprofile/",
-        //         "x": "https://x.com/yourhandle"
-        //     }
-        // }
-        // note: if you don't have a social, omit that key entirely.
-        // if you have no socials at all, you can omit the "socials" field.
-    ]
+  sites: [
+    {
+      name: "Anita Jiang",
+      year: "2028",
+      website: "https://anitajiang.me",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/antjng/",
+        x: "https://x.com/antjng_",
+      },
+    },
+    {
+      name: "Shadi El-Fares",
+      year: "2027",
+      website: "https://shadielfares.com",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/shadielfares/",
+        x: "https://x.com/shadielfares",
+      },
+    },
+    {
+      name: "Ayman Fouad",
+      year: "2026",
+      website: "https://ayman.fyi",
+      socials: {
+        linkedin: "https://www.linkedin.com/in/aymanfsm/",
+        x: "https://x.com/aymanfsm",
+      },
+    },
+    // add new sites above this comment
+    // for clarity: year refers to your projected graduation year.
+    // follow this format:
+    // {
+    //     name: "Your Name",
+    //     year: "20XX",
+    //     website: "https://yourwebsite.com",
+    //     socials: {
+    //         linkedin: "https://www.linkedin.com/in/yourprofile/",
+    //         x: "https://x.com/yourhandle"
+    //     }
+    // }
+    // note: if you don't have a social, omit that key entirely.
+    // if you have no socials at all, you can omit the "socials" field.
+  ],
 };
 
 // init site arrays
